0a03545751f044d3c4f88c0054ff246756e43695
Emil Ravn <emilravn@outlook.com>
2021-04-22 10:09:09 +0200

    clean

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="static/templates")
 
 @app.route("/")
-def index():
+def homepage():
     return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+host = "0.0.0.0"
+port = 5010
 
-# Kør direkte fra Python uden at initiere flask.
+# Kør direkte fra Python uden at initiere flask
 if __name__ == "__main__":
-    print("Serveren kører på localhost!")
-    app.run()
+    print(f"Serveren kører på localhost på port {port}.")
+    app.run(host=host, port=port,debug=True) # Vi sætter i debug mode for ikke at skulle genstarte programmet hver gang vi laver en ændring
