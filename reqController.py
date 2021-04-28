2be70bf48d2cd16ea613a41c0a65f693ee0030fa
LasseRSoerensen <61781595+SquintyShip@users.noreply.github.com>
2021-04-28 15:23:05 +0200

    cleaned up code

--- reqController.py
+++ reqController.py
@@ -1,57 +1,35 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify 
-#from testproject.spiders.testspider import QuotesSpider
 from APIreq import APIHandler, DataHandler
 import pandas as pd
 
 app = Flask(__name__)
 api_handler = APIHandler()
 data_handler = DataHandler()
-sentiment = []
-
-data2 = {'data_list': [], 'current_state': 0}
-#current = {'current_state': 0}
 
 
+data = {'data_list': [], 'current_state': 0}
 
-"""
-@app.route("/", methods=["POST", "GET"])
-def home():
-    
-    if request.method == "POST":
-        user = request.form["nm"]
-        query_result = api_handler.runHandler(user)
-        data2['data_list'] = data_handler.runHandler(query_result)
-        
-        
-    return render_template("index.html", data_t=data2, current_t = current)
-"""
 
+
+
+#connects index page with the backend
 @app.route('/', methods=["POST", "GET"])
 def test2():
     
-    data2['current_state'] = 0
+    data['current_state'] = 1
     if request.method == "POST":
-        user = request.form["nm"]
-        query_result = api_handler.runHandler(user)
-        data2['data_list'] = data_handler.runHandler(query_result)
-        data2['current_state'] = 1
-        return render_template("index2.html", data_t=data2, text=user)
+        user_search = request.form["nm"]
+        query_result = api_handler.runHandler(user_search)
+        data['data_list'] = data_handler.runHandler(query_result)
+        data['current_state'] = 1
+        return render_template("index2.html", data_t=data, text=user_search)
     
     
-    return render_template("index2.html", data_t=data2)
+    return render_template("index2.html", data_t=data)
 
 
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        query_result = api_handler.runHandler(user)
-        sentiment = data_handler.runHandler(query_result)
 
-        return render_template("result.html", content=sentiment)
-    else:
-        return render_template("login.html")
 
 @app.route("/<usr>")
 def user(usr):
--- static/js/app2.js
+++ static/js/app2.js
@@ -1,7 +1,4 @@
 
-
-
-
 new fullpage('#fullpage', {
     autoScrolling: true,
     onLeave: (origin, destination, direction) => {
@@ -13,14 +10,12 @@ new fullpage('#fullpage', {
 });
 
 fullpage_api.setScrollingSpeed(2000);
-	
-var currentURL = window.location.href;
 fullpage_api.setAllowScrolling(false);
 
 
 
 const btn1 = document.querySelector('#btn');
-btn1.onclick = () => enterSearch();
+//btn1.onclick = () => enterSearch();
 
 const btn2 = document.querySelector('#btn2');
 btn2.onclick = () => goAgain();
@@ -33,7 +28,7 @@ const btn = document.querySelector('#databtn');
 btn.onclick = () => alert(testdata);
 
 const btn22 = document.querySelector('#btn22');
-btn22.onclick = () => enterSearch();
+//btn22.onclick = () => enterSearch();
 
 
 if (test2 == 1){
@@ -41,74 +36,11 @@ if (test2 == 1){
 }
 
 
-const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
-
-async function waitFunc() {
-
-    let count = 0;
-
-    await sleepNow(7000)
-    fullpage_api.moveSectionDown();
-  /*
-    if (currentURL == "http://127.0.0.1:5000/test2#") {
-        
-        alert(count);
-        count++;
-    }
-*/
-/*
-    await sleepNow(3000)
-    if (currentURL == "http://127.0.0.1:5000/test2#") {
-        alert(count);
-        count++;
-    }
-
-    await sleepNow(3000)
-    if (currentURL == "http://127.0.0.1:5000/test2#") {
-        alert(count);
-        count++;
-    } else {
-        alert("no");
-    }
-*/
-}
-
-
-
-let dict = {};
-
-
-function countData(input) { 
-    
-
-    for (let index = 0; index < input.length; index++) {
-        if (dict[input[index]] == null) {
-            dict[input[index]] = 1;
-        
-        } else {
-            dict[input[index]]++;
-        }
-        
-    }
-
-}
-
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 
 let stringArray = testdata.map(String)
 
 function countAmount(input) {
     let categories = [0, 0, 0]; 
-    let categories_labels = ["Negative", "Neutral", "Positive"]; 
     for (let index = 0; index < input.length; index++) {
         if (input[index] >= 0.05) {
             categories[2]++;
@@ -117,43 +49,9 @@ function countAmount(input) {
         } else if (input[index] <= -0.05) {
             categories[0]++;
         }
-        
     }
-
     return categories;
 }
-
-
-
-/*
-
-def presentData(self):
-categories = [0, 0, 0]
-categories_labels = ['Negative', 'Neutral', 'Positive']
-explode = (0.1, 0.1, 0.1)
-
-for t in self.__tweet_sentiment_score:
-    print(t)
-    if t >= 0.05:
-        categories[2] += 1
-    elif t > -0.05 and t < 0.05:
-        categories[1] += 1
-    elif t <= -0.05:
-        categories[0] += 1
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
 
 
 let chart = document.getElementById('chart').getContext('2d'); 
@@ -228,15 +126,9 @@ let canvaschart3 = new Chart(canvas3, {
             label: 'Numbers',
             data: countAmount(testdata), 
             backgroundColor: [
-
                 'rgba(255, 0, 0, 0.6)',     
                 'rgba(255, 191, 0, 0.6)',
-                'rgba(0, 255, 64, 0.6)'
-
-
-
-
-                
+                'rgba(0, 255, 64, 0.6)'           
             ]
         }]
     }, 
@@ -253,57 +145,10 @@ let canvaschart3 = new Chart(canvas3, {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-let chart2 = document.getElementById('chart2').getContext('2d'); 
-//chart2.canvas.width = 200;
-//chart2.canvas.height= 200;
-let valueChart = new Chart(chart2, {
-    type: 'bar', 
-    data: {
-        labels: ['one', 'two', 'three', 'four'], 
-        datasets: [{
-            label: 'Numbers',
-            data: [1, 2, 3, 4]
-        }]
-    }, 
-    options: {
-        responsive: false,
-        maintainAspectRatio: false,
-        animation: {
-            duration: 2500, 
-        }
-    }
-})
-
-*/
-
-
 function goAgain() {
     fullpage_api.moveSectionUp()
 
 }
 
-function enterSearch() { 
-
-    //sleepNow(5000);
-    //waitFunc();
-    //fullpage_api.moveSectionDown();
-
-
-    
-}
 
 
