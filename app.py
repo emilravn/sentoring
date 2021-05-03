2410733f85d7a2e4f369cdd6233532b5775a3314
Emil Ravn <emilravn@outlook.com>
2021-05-03 14:57:09 +0200

    renames and routes to other pages

--- app.py
+++ app.py
@@ -6,36 +6,25 @@ app = Flask(__name__)
 api_handler = APIHandler()
 data_handler = DataHandler()
 
-
 #connects index page with the backend
 @app.route('/', methods=["POST", "GET"])
-def test2():
+def analyzer():
     data = {"tweet_data": {}, "current_state": 0}
-    
     if request.method == "POST":
         user_search = request.form["nm"]
         query_result = api_handler.runHandler(user_search)
         data['tweet_data'] = data_handler.runHandler(query_result)
         data['current_state'] = 1
         return render_template("index.html", data_t=data, text=user_search)
-    
-    
     return render_template("index.html", data_t=data)
 
 @app.route('/about')
 def about():
     return render_template("about-us.html")
 
-
-
-
-
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
-
-
-
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
